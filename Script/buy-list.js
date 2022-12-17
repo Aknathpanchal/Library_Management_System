7a9f62c77d53d055fda32d9ef5a817ba765826f1
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2022-12-17 22:19:14 +0530

    buy-list done

---
+++ Script/buy-list.js
@@ -0,0 +1,42 @@
+var datas=JSON.parse(localStorage.getItem("buy-list"))
+var booklist=JSON.parse(localStorage.getItem("book-list"))||[];
+
+datas.map(function(elem,i){
+    var row=document.createElement("tr")
+    var data1=document.createElement("td")
+    data1.innerText=elem.name;
+    var data2=document.createElement("td")
+    data2.innerText=elem.auther;
+    var data3=document.createElement("td")
+    data3.innerText=elem.desc;
+    var data4=document.createElement("td")
+    data4.innerText=elem.date;
+    var data5=document.createElement("td")
+    data5.innerText=elem.category;
+    var data6=document.createElement("td")
+    data6.innerText=elem.price;
+    var btn=document.createElement("button")
+        btn.setAttribute("id","removelist")
+        btn.innerText="remove"
+        btn.addEventListener("click",function(){
+            objcom={
+
+                name:elem.name,
+                auther:elem.auther,
+                desc:elem.desc,
+                date:elem.date,
+                category:elem.category,
+                price:elem.price,
+        
+            }
+            booklist.push(objcom)
+            localStorage.setItem("book-list",JSON.stringify(booklist));
+            datas.splice(i,1)
+            localStorage.setItem("buy-list",JSON.stringify(datas));
+            row.remove()
+        })
+
+row.append(data1,data2,data3,data4,data5,data6,btn)
+document.querySelector("tbody").append(row)
+
+})
