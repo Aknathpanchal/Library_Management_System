53a8e3c4c480a70574f5a4e96d766caa12629109
Aknathpanchal <100464802+Aknathpanchal@users.noreply.github.com>
2023-07-14 23:34:44 +0530

    Update format and add some info.

--- Script/database.js
+++ Script/database.js
@@ -1,107 +1,114 @@
-function handleFilter(){
- 
-    let value=document.getElementById('Filtertag').value;
-    if(value=='all'){
-      addProduct(datas)
-    }else{
-      let checkValue=datas.filter(function(el) {
-      return el.category===value ;
+/**
+ * The function "handleFilter" filters an array of data based on a selected value and adds the filtered
+ * data to a product list.
+ * @returns The function `handleFilter()` returns either all the products (`datas`) or a filtered array
+ * of products based on the selected category (`value`).
+ */
+function handleFilter() {
+  let value = document.getElementById("Filtertag").value;
+  if (value == "all") {
+    addProduct(datas);
+  } else {
+    let checkValue = datas.filter(function (el) {
+      return el.category === value;
     });
-    addProduct(checkValue)
-    }
+    addProduct(checkValue);
   }
+}
 
-  function sortByPrice() {
-    datas.map(function (elem) {
-        var sorting = document.querySelector("#sorter").value;
-        console.log(sorting);
-        if (sorting == "l2h") {
-            datas.sort(function (a, b) { return a.price - b.price })
-            appendProduct(datas)
-        }
-        else if (sorting == "h2l") {
-            datas.sort(function (a, b) { return b.price - a.price })
-            productappend(datas)
-        }
+/**
+ * The function sorts an array of data objects by price in either ascending or descending order based
+ * on the selected value of a dropdown menu.
+ * @returns The function does not explicitly return anything.
+ */
+function sortByPrice() {
+  datas.map(function (elem) {
+    var sorting = document.querySelector("#sorter").value;
+    console.log(sorting);
+    if (sorting == "l2h") {
+      datas.sort(function (a, b) {
+        return a.price - b.price;
+      });
+      appendProduct(datas);
+    } else if (sorting == "h2l") {
+      datas.sort(function (a, b) {
+        return b.price - a.price;
+      });
+      productappend(datas);
+    }
+  });
+}
 
-    })}
+var datas = JSON.parse(localStorage.getItem("book-list")) || [];
+var buylist = JSON.parse(localStorage.getItem("buy-list")) || [];
+var bookmark = JSON.parse(localStorage.getItem("bookmark-list")) || [];
 
+/* The code block is using the `map()` function to iterate over each element in the `datas` array. For
+each element, it creates a new table row (`tr`) and populates it with data from the element. It
+creates table data (`td`) elements for each property of the element (name, author, description,
+date, category, price). */
+datas.map(function (elem, i) {
+  var row = document.createElement("tr");
+  var data1 = document.createElement("td");
+  data1.innerText = elem.name;
+  var data2 = document.createElement("td");
+  data2.innerText = elem.auther;
+  var data3 = document.createElement("td");
+  data3.innerText = elem.desc;
+  var data4 = document.createElement("td");
+  data4.innerText = elem.date;
+  var data5 = document.createElement("td");
+  data5.innerText = elem.category;
+  var data6 = document.createElement("td");
+  data6.innerText = elem.price;
 
-var datas=JSON.parse(localStorage.getItem("book-list"))||[];
+  var btn = document.createElement("button");
+  btn.setAttribute("id", "addlist");
+  btn.innerText = "Buy";
+  btn.addEventListener("click", function () {
+    objcom = {
+      name: elem.name,
+      auther: elem.auther,
+      desc: elem.desc,
+      date: elem.date,
+      category: elem.category,
+      price: elem.price,
+    };
+    buylist.push(objcom);
 
-console.log(datas)
+    localStorage.setItem("buy-list", JSON.stringify(buylist));
 
-var buylist=JSON.parse(localStorage.getItem("buy-list"))||[];
-var bookmark=JSON.parse(localStorage.getItem("bookmark-list"))||[];
-datas.map(function(elem,i){
+    datas.splice(i, 1);
+    localStorage.setItem("book-list", JSON.stringify(datas));
+    row.remove();
+  });
 
-var row=document.createElement("tr")
-var data1=document.createElement("td")
-data1.innerText=elem.name;
-var data2=document.createElement("td")
-data2.innerText=elem.auther;
-var data3=document.createElement("td")
-data3.innerText=elem.desc;
-var data4=document.createElement("td")
-data4.innerText=elem.date;
-var data5=document.createElement("td")
-data5.innerText=elem.category;
-var data6=document.createElement("td")
-data6.innerText=elem.price;
+  var data7 = document.createElement("td");
+  data7.append(btn);
 
+  var btn1 = document.createElement("button");
+  btn1.setAttribute("id", "addbookmark");
+  btn1.innerText = "Bookmark";
+  btn1.addEventListener("click", function () {
+    objcom = {
+      name: elem.name,
+      auther: elem.auther,
+      desc: elem.desc,
+      date: elem.date,
+      category: elem.category,
+      price: elem.price,
+    };
+    bookmark.push(objcom);
 
-var btn=document.createElement("button")
-btn.setAttribute("id","addlist")
-btn.innerText="Buy"
-btn.addEventListener("click",function(){
-    objcom={
+    localStorage.setItem("bookmark-list", JSON.stringify(bookmark));
 
-                name:elem.name,
-                auther:elem.auther,
-                desc:elem.desc,
-                date:elem.date,
-                category:elem.category,
-                price:elem.price,
-        
-            }
-            buylist.push(objcom)
-        
-            localStorage.setItem("buy-list",JSON.stringify(buylist))
-        
-            datas.splice(i,1)
-            localStorage.setItem("book-list",JSON.stringify(datas));
-            row.remove()
-})
+    datas.splice(i, 1);
+    localStorage.setItem("book-list", JSON.stringify(datas));
+    row.remove();
+  });
+  var data8 = document.createElement("td");
+  data8.append(btn1);
 
-var data7=document.createElement("td")
-data7.append(btn)
-
-var btn1=document.createElement("button")
-btn1.setAttribute("id","addbookmark")
-btn1.innerText="Bookmark"
-btn1.addEventListener("click",function(){
-    objcom={
-
-        name:elem.name,
-        auther:elem.auther,
-        desc:elem.desc,
-        date:elem.date,
-        category:elem.category,
-        price:elem.price,
-            }
-        bookmark.push(objcom)
-        
-            localStorage.setItem("bookmark-list",JSON.stringify(bookmark))
-        
-            datas.splice(i,1)
-            localStorage.setItem("book-list",JSON.stringify(datas));
-            row.remove()
-})
-var data8=document.createElement("td")
-data8.append(btn1)
-
-row.append(data1,data2,data3,data4,data5,data6,data7,data8)
-document.querySelector("tbody").append(row)
-
-
-})
+  row.append(data1, data2, data3, data4, data5, data6, data7, data8);
+  document.querySelector("tbody").append(row);
+});
--- Script/index.js
+++ Script/index.js
@@ -1,28 +1,34 @@
 var data;
-data=JSON.parse(localStorage.getItem("book-list"))||[];
-document.querySelector("#form").addEventListener("submit",getdata);
+data = JSON.parse(localStorage.getItem("book-list")) || [];
+document.querySelector("#form").addEventListener("submit", getdata);
 
-function getdata(event){
-    event.preventDefault();
+/**
+ * The function `getdata` is used to retrieve data from form inputs, create an object with the data,
+ * store it in local storage, and log the data to the console.
+ * @param event - The event parameter is an object that represents the event that triggered the
+ * function. In this case, it is used to prevent the default behavior of a form submission, which is to
+ * refresh the page. By calling `event.preventDefault()`, the function prevents the form from being
+ * submitted and the page from being refreshed
+ */
+function getdata(event) {
+  event.preventDefault();
 
+  var name = document.querySelector("#name").value;
+  var auther = document.querySelector("#auther").value;
+  var desc = document.querySelector("#desc").value;
+  var date = document.querySelector("#date").value;
+  var category = document.querySelector("#category").value;
+  var price = document.querySelector("#price").value;
 
-    var name =document.querySelector("#name").value;
-    var auther =document.querySelector("#auther").value;
-    var desc =document.querySelector("#desc").value;
-    var date=document.querySelector("#date").value;
-    var category=document.querySelector("#category").value;
-    var price=document.querySelector("#price").value;
-
-    var objdata={
-        name:name,
-        auther:auther,
-        desc:desc,
-        date:date,
-        category:category,
-        price:price
-    }
-    data.push(objdata)
-    localStorage.setItem("book-list",JSON.stringify(data))
-    console.log(data)
+  var objdata = {
+    name: name,
+    auther: auther,
+    desc: desc,
+    date: date,
+    category: category,
+    price: price,
+  };
+  data.push(objdata);
+  localStorage.setItem("book-list", JSON.stringify(data));
+  console.log(data);
 }
-console.log(data)
